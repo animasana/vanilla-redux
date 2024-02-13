a19ddd6c2e7f24c4336025ee560e600d3f2e4c2f
animasana <animagig@gmail.com>
2024-02-14 00:05:03 +0900

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { legacy_createStore } from "redux";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const plus = document.getElementById("plus");
+const minus = document.getElementById("minus");
+const number = document.querySelector("span");
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const ActionType = {
+  PLUS: "PLUS",
+  MINUS: "MINUS",
+}
+
+number.innerText = 0;
+
+function countModifier(count = 0, action) {
+  switch (action.type) {
+    case ActionType.PLUS:
+      return ++count;
+    case ActionType.MINUS:
+      return --count;
+    default:
+      return count;
+  }  
+}
+const countStore = legacy_createStore(countModifier);
+
+function handlePlus() {
+  countStore.dispatch({type: ActionType.PLUS});
+}
+
+function handleMinus() {
+  countStore.dispatch({type: ActionType.MINUS});
+}
+
+plus.addEventListener("click", handlePlus);
+minus.addEventListener("click", handleMinus);
+
+
+
+function onChange() {
+  number.innerText = countStore.getState();
+}
+countStore.subscribe(onChange)
+
